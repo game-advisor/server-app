33930b80d878602ced93acae6158fc103e75b4ae
Zajcw <exaner9@gmail.com>
2021-10-26 13:14:44 +0200

    Initial commit

---
+++ src/main/java/inz/gameadvisor/restapi/RestapiApplication.java
@@ -0,0 +1,13 @@
+package inz.gameadvisor.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiApplication.class, args);
+	}
+
+}
