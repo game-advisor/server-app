e24fdbe93601fd4b223af7d40f2f0129753062d1
Zajcw <82063063+Zajcw@users.noreply.github.com>
2021-12-22 00:41:54 +0100

    Added field publishDate to game entity (to reflect current database state)

--- src/main/java/inz/gameadvisor/restapi/model/Game.java
+++ src/main/java/inz/gameadvisor/restapi/model/Game.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Getter
 @Setter
@@ -21,4 +22,7 @@ public class Game {
     private Companies company;
 
     private String imagePath;
+
+    @Column(name = "publishDate")
+    private Date publishDate;
 }
